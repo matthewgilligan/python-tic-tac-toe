369bae597d257df330473d971b8c0113651575c4
Matthew Gilligan <mcgilligan97@gmail.com>
2020-06-10 19:33:59 +0100

    finishing number replacement game

--- warm-up-exercises/simple_user_interaction.py
+++ warm-up-exercises/simple_user_interaction.py
@@ -1,13 +1,28 @@
-game_list = [0, 1, 2]
-
-
 def display_game(game_list):
   print("Here is the current list")
   print(game_list)
 
 
+def gameon_choice():
+  choice = 'wrong'
+
+  while choice not in ['Y', 'N']:
+    choice = input('Do you want to carry on playing? (Y or N) ')
+
+    if choice not in ['Y', 'N']:
+      print('Please choose either Y or N ')
+
+  return choice == 'Y'
+
+
 def replacement_choice(game_list, position):
-  replacement = input("What would you like to replace it with? ")
+  replacement = int(input("What would you like to replace it with? "))
+
+  for n, i in enumerate(game_list):
+    if i == position:
+      game_list[n] = replacement
+
+  return game_list
 
 
 def position_choice():
@@ -23,13 +38,22 @@ def position_choice():
     if choice.isdigit():
       if int(choice) in game_list:
         included = True
-        replacement_choice(game_list, int(choice)
+        return int(choice)
       else:
+        included = False
         print("Sorry, that is not an available number!")
-        included=False
-
-# def gameon_choice():
 
 
-display_game(game_list)
-position=position_choice()
+game_on = True
+game_list = [0, 1, 2]
+
+while game_on:
+  display_game(game_list)
+
+  position = position_choice()
+
+  game_list = replacement_choice(game_list, position)
+
+  display_game(game_list)
+
+  game_on = gameon_choice()
